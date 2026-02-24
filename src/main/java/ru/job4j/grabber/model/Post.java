2e9d399f16f3b63507667fdfc12f4e4cd778bec4
Pavel Matveev KZN <pmatveev@dpd.ru>
2026-02-24 19:28:49 +0300

    Модель данных - Post. [#285211 #558547]

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -6,6 +6,20 @@ public class Post {
     private Long id;
     private String title;
     private String link;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(link);
+    }
+
     private String description;
     private Long time;
 
@@ -47,23 +61,6 @@ public class Post {
 
     public void setTime(Long time) {
         this.time = time;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Post post = (Post) o;
-        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, link, description);
     }
 
     @Override
