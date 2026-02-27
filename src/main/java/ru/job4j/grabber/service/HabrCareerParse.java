4a1bd83840968e617672a38ac0fa57f92ec8e938
Pavel Matveev KZN <pmatveev@dpd.ru>
2026-02-27 17:40:48 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
@@ -0,0 +1,50 @@
+package ru.job4j.grabber.service;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import ru.job4j.grabber.model.Post;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HabrCareerParse implements Parse {
+    private static final Logger log = Logger.getLogger(HabrCareerParse.class);
+    private static final String SOURCE_LINK = "https://career.habr.com";
+    private static final String PREFIX = "/vacancies?page=";
+    private static final String SUFFIX = "&q=Java%20developer&type=all";
+
+    @Override
+    public List<Post> fetch() {
+        var result = new ArrayList<Post>();
+        try {
+            int pageNumber = 1;
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            var connection = Jsoup.connect(fullLink);
+            var document = connection.get();
+            var rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                var titleElement = row.select(".vacancy-card__title").first();
+                var linkElement = titleElement.child(0);
+                var dateElement = row.select(".basic-date").first();
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK,
+                        linkElement.attr("href"));
+                String date = dateElement.attr("datetime");
+                var post = new Post();
+                post.setTitle(vacancyName);
+                post.setLink(link);
+                post.setTime(Instant.parse(date).toEpochMilli());
+                result.add(post);
+            });
+        } catch (IOException e) {
+            log.error("When load page", e);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        var v = new HabrCareerParse().fetch();
+        System.out.println("Прив");
+    }
+}
